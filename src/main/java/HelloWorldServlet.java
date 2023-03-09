dff64162ce6b57aea6d02b3e32e56aa3fc564f99
Reginald Graham <reg.graham98@gmail.com>
2023-03-09 11:36:59 -0600

    say hello and countServlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,31 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    @Override
 
+//        protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//            res.getWriter().println("<div id='count'></div><script> let i = localStorage.getItem('count') || 0;\n" +
+//                    "  i++;\n" +
+//                    "  localStorage.setItem('count', i);let numCount=document.querySelector('#count'); numCount.innerHTML=i</script>");
+//        }
+//    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        res.getWriter().println("<div>Hello World</div>");
+        PrintWriter pw=res.getWriter();
+        String word= req.getParameter("name");
+        if (word == null){
+            pw.println("hello world");
+        }else{
+            pw.println("hello " + word + ".");
+        }
     }
 }
+
+
