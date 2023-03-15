4ab6a29e65fdd13aff174ae8780ce50042d87728
Reginald Graham <reg.graham98@gmail.com>
2023-03-15 11:07:31 -0500

    say hello and countServlet

---
+++ src/main/java/ProfileServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "ViewProfileServlet", urlPatterns = "/profile")
+public class ProfileServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if(session.getAttribute("user") != null){
+            String username = (String) session.getAttribute("user");
+            request.setAttribute("name", username);
+            request.getRequestDispatcher("WEB-INF/profile.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
