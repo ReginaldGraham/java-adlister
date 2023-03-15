280809536fcd755f596cfa3905c025c975c88cab
Reginald Graham <reg.graham98@gmail.com>
2023-03-15 00:59:35 -0500

    say hello and countServlet

--- src/main/java/controllers/loginServlet.java
+++ src/main/java/controllers/loginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "login", urlPatterns = "/login")
@@ -14,14 +15,20 @@ public class loginServlet extends HttpServlet {
       request.getRequestDispatcher("login.jsp").forward(request,response);
    }
    @Override
-   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, NullPointerException{
       if (request.getMethod().equalsIgnoreCase("post")) {
          String username = request.getParameter("username");
          String password = request.getParameter("password");
+            HttpSession session = request.getSession();
+         if(session.getAttribute("user") != null && session.getAttribute("user").equals("user")){
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+         }else {
          if (username.equals("admin") && password.equals("password")) {
-            response.sendRedirect("/profile.jsp");
+            request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
+             session.setAttribute("user",true);
          }else { response.sendRedirect("/login");
 
+         }
          }
       }
    }
